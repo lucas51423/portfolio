6556b331906d2f0bbb975c317cfc3f918d9049fb
lucas51423 <lukinhaskbh1@gmail.com>
2025-05-28 15:25:00 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,6 +18,12 @@ const projetos = [
     descricao: "Um site que busca a imagem de perfil do GitHub com base no nome de usuário informado, desenvolvido com JavaScript moderno.",
     imagem: "https://www.bleepstatic.com/content/hl-images/2022/04/08/GitHub__headpic.jpg",
     link: "projetos/busca_github/index.html"
+},
+  {
+    titulo: "Projeto Cão e Gato",
+    descricao: "Este foi o primeiro projeto que desenvolvi como parte dos meus estudos, utilizando HTML e CSS. Um site simples sobre cudados profissionais com Cães e gatos.",
+    imagem: "https://thumbs.dreamstime.com/b/gato-e-cachorro-bonitos-em-fundo-amarelo-c%C3%A3o-bonitinho-250093296.jpg",
+    link: "projetos/cao_e_gato/index.html"
 }
 ]
 
